21be578901cd9811f49d136b95c2b37246ef3f8d
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-04 03:37:26 +0400

    2 Task

---
+++ TaskA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Design;
+
+int[] arr = { 1, 24 ,5,12,6,72,46,2,4};
+int numbers = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] <= 9 && arr[i] >= 0)
+    {
+        numbers++;
+    }
+}
+Console.WriteLine( numbers + "  reqem var");
